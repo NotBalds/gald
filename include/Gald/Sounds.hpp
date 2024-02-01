23d0d102ca0f6244d0ff0e9f666e56ac07f22c91
ProggerX <x@proggers.ru>
2024-02-01 10:56:30 +0300

    Fix govnocode

--- include/Gald/Sounds.hpp
+++ include/Gald/Sounds.hpp
@@ -8,27 +8,23 @@ using namespace std;
 namespace Gald {
 class SoundPlayer {
 	string _path;
-	bool _is_playing;
 	sf::Sound* _sound;
 	sf::SoundBuffer* _buffer;
 
 public:
 	SoundPlayer(string path) {
 		_path = path;
-		_is_playing = false;
 		_buffer = new sf::SoundBuffer;
 		_buffer->loadFromFile(path.c_str());
 		_sound = new sf::Sound(*_buffer);
 	}
 	void play() {
-		bool is_playing = true;
 		_sound->play();
 	}
 	bool is_playing() {
 		return (_sound->getStatus() == _sound->Playing ? 1 : 0);
 	}
 	void stop() {
-		bool is_playing = false;
 		_sound->stop();
 	}
 	~SoundPlayer() {
